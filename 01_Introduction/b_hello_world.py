68e437ff933ce04d426d163d56f271bbef7ea0aa
Harshini3108 <harshini.k3108@gmail.com>
2024-11-14 07:49:31 +0000

    class02-introduction to python

---
+++ 01_Introduction/b_hello_world.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+Purpose: Basic PEP 8 Guidelines and Shebang line
+
+PEP : Python Enhanching Proposal
+PEP 8 : Coding style guide
+
+This is docstring(document string)
+
+"""
+
+# print statement in Python 2.x
+# print "Hello world"
+
+# Print statement in Python 3.X
+print("Hello World!")
+print("True")
+
+def wishes(name):
+    wish = "How are you?". format(name)
+    print(wish)
+
+wishes("Harshini")
